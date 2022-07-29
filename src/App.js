0e522e8f5f2d888077e95b6933f0882b4da64416
dadafly1244 <jc1525@hanmail.net>
2022-07-29 14:11:23 +0900

    feat: feat Timer with useEffect

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
+import Timer from './components/Timer';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState('');
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-  const handleInputChange = (e) => {
-    setName(e.target.value);
-  };
-
-  //렌더링될때마다 매번 실행 -> 렌더링이후
-  useEffect(() => {
-    console.log('렌더링');
-  });
-  //마운팅 + count가 변화할때마다 실행됨
-  useEffect(() => {
-    console.log('count');
-  }, [count]);
-
-  //마운팅 + name가 변화할때마다 실행됨
-  useEffect(() => {
-    console.log('name');
-  }, [name]);
-
-  //마운트 될때만 실행
-  useEffect(() => {
-    console.log('마운트');
-  }, []);
-
+  const [showTimer, setShowTimer] = useState(false);
   return (
     <div>
-      <div>
-        <button onClick={handleClick}>증가~</button>
-        <span>{count}</span>
-      </div>
-      <div>
-        <input type="text" value={name} onChange={handleInputChange} />
-        <span>{name}</span>
-      </div>
+      {showTimer && <Timer />}
+      <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
     </div>
   );
 }
